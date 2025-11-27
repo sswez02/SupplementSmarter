52d8a7499feb3d1d23beea796596c8a8f9f2e3b7
Sam <sw.samuel.10@gmail.com>
2025-11-27 13:07:32 +1300

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import { app } from './app.js';
 
 const PORT = process.env.PORT ?? 4000;
+console.log(`PORT is set to: ${PORT}`);
 
 app.listen(PORT, () => {
   console.log(`API listening on http://localhost:${PORT}`);
